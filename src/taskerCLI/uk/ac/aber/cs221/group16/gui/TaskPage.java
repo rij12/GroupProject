6c35a950af87a4be863514d8abf72b4b0b141b24
rij12 <rij12@aber.ac.uk>
2015-11-04 15:19:42 +0000

    Update TaskPage.java
    Corrected the source code to match class

--- src/taskerCLI/uk/ac/aber/cs221/group16/gui/TaskPage.java
+++ src/taskerCLI/uk/ac/aber/cs221/group16/gui/TaskPage.java
@@ -1,104 +1,103 @@
 package gui;
 
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import net.miginfocom.swing.MigLayout;
-import javax.swing.JTextPane;
-import javax.swing.JTextArea;
-import javax.swing.JList;
-import javax.swing.JTree;
-import javax.swing.JEditorPane;
-import javax.swing.JToggleButton;
-import javax.swing.JTextField;
-import javax.swing.JRadioButton;
-import javax.swing.ButtonGroup;
-import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
-import javax.swing.Action;
-import javax.swing.JButton;
 import java.awt.event.ActionListener;
-/**
- * <p>
- * This class generate the graphical user interface for the edit task window. 
- * The following changes still need to be made.
- * Action listeners.
- * Test for bugs
- * Frame disposing 
- * </P>
- * @version 1.0
- * @author Richard Price-Jones 
- * 
- */
-public class TaskEditor extends JFrame {
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import net.miginfocom.swing.MigLayout;
+
+public class TaskPage extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField txtCurrentTask;
-	private final ButtonGroup buttonGroup = new ButtonGroup();
-
+	private JTextField txtSearchField;
+	private JTable table;
+	private JButton editTaskButton;
 
 	/**
 	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					TaskEditor frame = new TaskEditor();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+//	 */
+//	public static void main(String[] args) {
+//		EventQueue.invokeLater(new Runnable() {
+//			public void run() {
+//				try {
+//					TaskPage frame = new TaskPage();
+//					frame.setVisible(true);
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		});
+//	}
 
 	/**
 	 * Create the frame.
 	 */
-	public TaskEditor() {
+	public TaskPage() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 935, 478);
+		setBounds(100, 100, 978, 550);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
 		
 		JPanel panel = new JPanel();
-		// layout manager for the frame
 		contentPane.add(panel, BorderLayout.CENTER);
-		// seting setlout magager for the panel
-		panel.setLayout(new MigLayout("", "[grow][grow]", "[][][][][][grow][]"));
-		// Current user Display
-		txtCurrentTask = new JTextField();
-		txtCurrentTask.setEditable(false);
-		txtCurrentTask.setText("Current Task");
-		panel.add(txtCurrentTask, "cell 0 1,alignx center,aligny center");
-		txtCurrentTask.setColumns(10);
-		// Radio buttons
-		// need to get data from the database
-		JRadioButton completedadioButton = new JRadioButton("Completed\r\n");
-		buttonGroup.add(completedadioButton);
-		panel.add(completedadioButton, "flowx,cell 1 1,alignx center,aligny center");
+		panel.setLayout(new MigLayout("", "[][][grow]", "[][grow][]"));
 		
-		JRadioButton onGoingRadioButton = new JRadioButton("on-going");
-		buttonGroup.add(onGoingRadioButton);
-		panel.add(onGoingRadioButton, "cell 1 1,alignx center,aligny center");
-		//Text Box
-		// Needs database input
-		JEditorPane editorPane = new JEditorPane();
-		panel.add(editorPane, "cell 0 4 2 2,grow");
-		//submit button
-		JButton SubmitButton = new JButton("Submit\r\n");
-		SubmitButton.addActionListener(new ActionListener() {
+		JLabel lblCurrentUserInformation = new JLabel("Current user information");
+		panel.add(lblCurrentUserInformation, "cell 0 0,alignx center,aligny center");
+		
+		txtSearchField = new JTextField();
+		txtSearchField.setText("Search Field");
+		panel.add(txtSearchField, "cell 2 0,alignx center");
+		txtSearchField.setColumns(10);
+		
+		JLabel lblCurrentTaskInformation = new JLabel("Current Task Information  -- hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh\n\nhhhhhhhhhhh"
+				+ "hhhhhhhhhhhhhhhh");
+		panel.add(lblCurrentTaskInformation, "cell 0 1");
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null, null, null},
+			},
+			new String[] {
+				"StartDate", "Comment", "Status", "Date of Completion", "Member Allocated", "Task ID"
+			}
+		) {
+			Class[] columnTypes = new Class[] {
+				String.class, String.class, String.class, String.class, String.class, Integer.class
+			};
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+		});
+		table.getColumnModel().getColumn(0).setPreferredWidth(96);
+		table.getColumnModel().getColumn(1).setPreferredWidth(125);
+		table.getColumnModel().getColumn(1).setMinWidth(50);
+		table.getColumnModel().getColumn(2).setPreferredWidth(90);
+		table.getColumnModel().getColumn(3).setPreferredWidth(121);
+		panel.add(table, "cell 2 1,alignx center,growy");
+		
+		editTaskButton = new JButton("New button");
+		editTaskButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 			}
 		});
-		panel.add(SubmitButton, "cell 0 6 2 1,alignx center,aligny center");
+		panel.add(editTaskButton, "cell 0 2,alignx center,aligny center");
+		
+		
+		
+		
 	}
 
-	
 }
